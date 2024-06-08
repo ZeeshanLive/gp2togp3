95aac2ce4afdfc11f0debd2ca9a9b46d0cd3121d
Your Name <you@example.com>
2024-06-09 00:35:40 +0530

    added logic to convert only gp2 volumes and Error Handling

--- gp2togp3.py
+++ gp2togp3.py
@@ -1,10 +1,21 @@
 #!/bin/python3
 import boto3
+from botocore.exceptions import ClientError
 volumes = boto3.client('ec2')
 paginator = volumes.get_paginator('describe_volumes')
 page_iterator = paginator.paginate()
 for page in page_iterator:
     for volume in page['Volumes']:
         print (volume['VolumeId'])
-        volumes.modify_volume(VolumeId= volume['VolumeId'],VolumeType='gp3')
+        VolumeInfo=volumes.describe_volumes(VolumeIds=[volume['VolumeId']])
+        if VolumeInfo['Volumes'][0]['VolumeType'] == 'gp3':
+            print (f"{volume['VolumeId']} is Already Gp3")
+        elif VolumeInfo['Volumes'][0]['VolumeType'] == 'gp2':
+            print (f"{volume['VolumeId']} is gp2 Converting to gp3")
+            try:
+                volumes.modify_volume(VolumeId= volume['VolumeId'],VolumeType='gp2')
+            except ClientError as e:
+                print("Unexpected error: %s" %e)
+        else:
+            print (f"dosen't fall under gp2 or gp3 {volume['VolumeId']}")
 
